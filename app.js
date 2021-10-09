96dac8bed8ad0f8ce09b07aa7964e21645d74b66
Suhyr Hasan <suhyrhasan@csu.fullerton.edu>
2021-10-08 17:14:43 -0700

    added a Spotify embed for music

--- app.js
+++ app.js
@@ -23,9 +23,9 @@ app.set('view engine', 'hbs');
 app.use('/', require('./routes/pages'));
 app.use('/auth', require('./routes/auth'));
 
-app.listen(5000, () => {
-    console.log('Server started on port 5000');
-});
+//app.listen(5000, () => {
+   // console.log('Server started on port 5000');
+//});
 
 app.listen(8888, () =>
   console.log(
---
+++ spotifyapi/getPlaylist.js
@@ -0,0 +1,40 @@
+const fs = require('fs')
+const SpotifyWebApi = require('spotify-web-api-node');
+
+//
+const token = access_token;
+
+const spotifyApi = new SpotifyWebApi();
+spotifyApi.setAccessToken(token);
+
+//GET  PROFILE DATA
+function getMyData() {
+  (async () => {
+    const me = await spotifyApi.getMe();
+    // console.log(me.body);
+    getUserPlaylists(me.body.id);
+  })().catch(e => {
+    console.error(e);
+  });
+}
+
+//GET  PLAYLISTS
+async function getUserPlaylists(userName) {
+  const data = await spotifyApi.getUserPlaylists(userName)
+
+  console.log("---------------+++++++++++++++++++++++++")
+  let playlists = []
+
+  for (let playlist of data.body.items) {
+    console.log(playlist.name + " " + playlist.id)
+    
+    let tracks = await getPlaylistTracks(playlist.id, playlist.name);
+    // console.log(tracks);
+
+    const tracksJSON = { tracks }
+    let data = JSON.stringify(tracksJSON);
+    fs.writeFileSync(playlist.name+'.json', data);
+  }
+}
+
+getMyData()
