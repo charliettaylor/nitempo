4216b4a5ecb7adf6e549ab144105c53754248aca
DESKTOP-BIT26TU\benmr <benmrtnz27@gmail.com>
2021-12-08 01:20:28 -0800

    12/8

--- frontend/src/pages/spotifyLogin/SpotifyLogin.jsx
+++ frontend/src/pages/spotifyLogin/SpotifyLogin.jsx
@@ -3,32 +3,17 @@ import { useEffect } from 'react';
 import "./SpotifyLogin.css";
 import axios from 'axios';
 import { Container } from "react-bootstrap";
-import { Link } from "react-router-dom";
 axios.defaults.headers.common['authorization'] = 'bearer y38fa8df5ad9402ebe4c131072cf4768';
 
+const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=dace8b06faeb4ce09cde9f32c8f55de9&response_type=code&redirect_uri=https://nitempo.herokuapp.com/auth/callback&scope=ugc-image-upload%20user-read-playback-state%20user-modify-playback-state%20user-read-currently-playing%20streaming%20app-remote-control%20user-read-email%20user-read-private%20playlist-read-collaborative%20playlist-modify-public%20playlist-read-private%20playlist-modify-private%20user-library-modify%20user-library-read%20user-top-read%20user-read-playback-position%20user-read-recently-played%20user-follow-read%20user-follow-modify"
 
 export default function SpotifyLogin() {
-  useEffect(() => {
-    const fetchUser = async () => {
-        try {
-            const res = await axios.get("/auth/spotifyLogin")
-            console.log(res.data.redirect)
-        } catch(e) {
-            console.error(e.response.data)
-        }
-    }
-    fetchUser();
-  }, [])
 
-  const handleClick = (e) => {
-    e.preventDefault()
-  }
-  
   return (
     <div>
-      <form onSubmit={handleClick}>
-          <button className="button" type="submit">Log In</button>
-      </form>
+      <a href={AUTH_URL}>
+        Login with Spotify
+      </a>
     </div>
   )
 }
